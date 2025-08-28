2069c86cb4b59746922e83cd09445850c57dd052
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2025-08-28 14:23:00 +0600

    fix the button issue and fix the innerHTML element background color

--- Emergency/emergency.js
+++ Emergency/emergency.js
@@ -67,7 +67,7 @@ getElement("survice-box").addEventListener("click", function (e) {
     const newCart = document.createElement("div");
 
     newCart.innerHTML = `
-         <div class="bg-gray-200 rounded-xl flex  justify-between p-4 my-5">
+         <div class="bg-[#fafafa] rounded-xl flex  justify-between p-4 my-5">
            <div class="">
                 <h2 class="font-bold">${serviceTitle}</h2>
                    <p class="">${serviceNumber}</p>
